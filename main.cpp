dc37b06097d074dad8cd21699574868e713900aa
Luke Kellner <lukedkellner@students.nic.edu>
2024-10-23 10:24:27 -0700

    maybe local

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 using namespace std;
-
+void say(string word);
 int main()
 {
     cout<<"Hello\n";
     cout<<"My name is Tom\n";
+    say("Hi");
     return 0;
+}
+void say(string word)
+{
+    cout<<word<<endl;
 }
