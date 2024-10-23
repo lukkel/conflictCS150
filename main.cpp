10f9ea99d5ebc490bd7cf6c47ee29326873a3c19
lukkel <ldkellner@students.nic.edu>
2024-10-23 17:19:40 +0000

    function online

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 using namespace std;
-
+int add(int a, int b);
 int main()
 {
     cout<<"My name is Tom\n";
+    cout<<"5 + 7 = "<<add(5,7)<<endl;
     return 0;
+}
+int add(int a, int b)
+{
+    return a+b;
 }
